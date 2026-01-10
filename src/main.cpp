d5af56884620bb5d377007176c8f401f6f340b10
Nikolai Poliarnyi <polarnick239@gmail.com>
2026-01-10 16:38:23 +0300

    main: solution to build background-foreground mask (using binary thresholding and morphology)

--- src/main.cpp
+++ src/main.cpp
@@ -25,30 +25,48 @@ int main() {
         std::cout << "image loaded in " << t.elapsed() << " sec" << std::endl;
         debug_io::dump_image("debug/00_input.jpg", image);
 
-        // TODO: convert image to grayscale and save it into debug file
+        image32f grayscale = to_grayscale_float(image);
+        rassert(grayscale.channels() == 1, 2317812937193);
+        rassert(grayscale.width() == w && grayscale.height() == h, 7892137419283791);
+        debug_io::dump_image("debug/01_grayscale.jpg", grayscale);
 
         std::vector<float> intensities_on_border;
-        // TODO: find grayscale intensities on borders - they are from dark background
         for (int j = 0; j < h; ++j) {
-            // TODO
+            for (int i = 0; i < w; ++i) {
+                // skip everything except borders
+                if (i != 0 && i != w - 1 && j != 0 && j != h - 1)
+                    continue;
+                intensities_on_border.push_back(grayscale(j, i));
+            }
         }
-
         // TODO what invariant we can check with rassert about intensities_on_border.size()?
-        rassert(intensities_on_border.size() >= 0, 7283197129381312);
+        rassert(intensities_on_border.size() == 2 * w + 2 * h - 4, 7283197129381312);
         std::cout << "intensities on border: " << stats::summaryStats(intensities_on_border) << std::endl;
 
         // TODO find background_threshold
-        double background_threshold = 0.0;
+        double background_threshold = 1.5 * stats::percentile(intensities_on_border, 90);
         std::cout << "background threshold=" << background_threshold << std::endl;
 
         // TODO build is_foreground_mask + save its visualization on disk + print into log percent of background
-        image8u is_foreground_mask;
+        image8u is_foreground_mask = threshold_masking(grayscale, background_threshold);
+        double is_foreground_sum = stats::sum(is_foreground_mask.toVector());
+        std::cout << "thresholded background: " << stats::toPercent(w * h - is_foreground_sum / 255.0, 1.0 * w * h) << std::endl;
+        debug_io::dump_image("debug/02_is_foreground_mask.png", is_foreground_mask);
 
-        // TODO print to console percent of foreground pixels
-        // TODO save mask visualization to debug file
-
+        t.restart();
         // TODO make mask more smooth thanks to morphology
         // TODO firstly try dilation + erosion, is the mask robust? no outliers?
+        int strength = 3;
+        image8u dilated_mask = morphology::dilate(is_foreground_mask, strength);
+        image8u dilated_eroded_mask = morphology::erode(dilated_mask, strength);
+        image8u dilated_eroded_eroded_mask = morphology::erode(dilated_eroded_mask, strength);
+        image8u dilated_eroded_eroded_dilated_mask = morphology::dilate(dilated_eroded_eroded_mask, strength);
+        is_foreground_mask = dilated_eroded_eroded_dilated_mask;
+        std::cout << "morphology in " << t.elapsed() << " sec" << std::endl;
+        debug_io::dump_image("debug/03_is_foreground_dilated.png", dilated_mask);
+        debug_io::dump_image("debug/04_is_foreground_dilated_eroded.png", dilated_eroded_mask);
+        debug_io::dump_image("debug/05_is_foreground_dilated_eroded_eroded.png", dilated_eroded_eroded_mask);
+        debug_io::dump_image("debug/06_is_foreground_dilated_eroded_eroded_dilated.png", dilated_eroded_eroded_dilated_mask);
 
         std::cout << "processed in " << total_t.elapsed() << " sec" << std::endl;
 
